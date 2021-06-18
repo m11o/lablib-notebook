9ee25e923bde22952745e559e159c606d2dce5e5
masahito1997 <velonica.gale2227@gmail.com>
2021-06-18 18:42:42 +0900

    add const file for sulfur

---
+++ utils/sulfur/constant.py
@@ -0,0 +1,9 @@
+ANIMAL_NUMBERS = [
+    'A', 'B', 'C', 'G', 'H', 'I', 'K', 'L', 'N', 'Q'
+]
+ANIMAL_NAMES = ['ID181106Cre%s' % animal_number for animal_number in ANIMAL_NUMBERS]
+
+CONTEXTS = [
+    'HC1', 'A1preES', 'A1postES', 'HC2', 'HC3', 'A2_1', 'A2_2',
+    'A3_1', 'A3_2', 'B1', 'HC4', 'A4preES', 'A4postES', 'HC5', 'HC6', 'A5', 'B2'
+]
