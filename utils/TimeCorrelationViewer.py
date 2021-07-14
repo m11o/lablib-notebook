8d054609a94846f587e357f7b709f33345ba50b5
masahito1997 <velonica.gale2227@gmail.com>
2021-07-14 12:31:41 +0900

    refactor for TimeCorrelationViewer.py

--- utils/TimeCorrelationViewer.py
+++ utils/TimeCorrelationViewer.py
@@ -11,110 +11,71 @@ class TimeCorrelationViewer:
     SCALE_MAX = 0.06
     HEATMAP_CMAP = 'RdYlBu_r'
 
+    DEFAULT_FONTSIZE = 20
+    DEFAULT_FONT_WEIGHT = 'bold'
+
     def __init__(self, shuffle_df, engram_df, non_engram_df):
         self.shuffle_df = shuffle_df.astype(float)
         self.engram_df = engram_df.astype(float)
         self.non_engram_df = non_engram_df.astype(float)
+        self.time_ticks = list(range(0, len(self.shuffle_df) + 1, 5))
 
     def view_heatmap(self):
-        _fig, axes = plt.subplots(ncols=3, sharey=True, figsize=(25, 7))
-        ax_for_all = sns.heatmap(self.shuffle_df, ax=axes[0], robust=True, vmin=self.SCALE_MIN, vmax=self.SCALE_MAX, square=True, cmap=self.HEATMAP_CMAP)
-        ax_for_all.invert_yaxis()
-        ax_for_engram = sns.heatmap(self.engram_df, ax=axes[1], robust=True, vmin=self.SCALE_MIN, vmax=self.SCALE_MAX, square=True, cmap=self.HEATMAP_CMAP)
+        fig, axes = plt.subplots(ncols=3, figsize=(25, 7), sharey=True, tight_layout=False, dpi=200)
+
+        ax_for_engram = sns.heatmap(self.engram_df, ax=axes[0], robust=True, vmin=self.SCALE_MIN, vmax=self.SCALE_MAX, square=True, cmap=self.HEATMAP_CMAP)
+        axes[0].set_xticks(self.time_ticks)
+        axes[0].set_xticklabels(self.time_ticks, fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+        axes[0].set_xlabel('Reference Time (s)', fontsize=self.DEFAULT_FONTSIZE)
+        axes[0].set_yticks(self.time_ticks)
+        axes[0].set_yticklabels(self.time_ticks, fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+        axes[0].set_ylabel('Compared Time (s)', fontsize=self.DEFAULT_FONTSIZE)
+        axes[0].set_title('Engram cells', fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+
+        color_bar_for_engram = ax_for_engram.collections[0].colorbar
+        color_bar_for_engram.ax.tick_params(labelsize=self.DEFAULT_FONTSIZE)
         ax_for_engram.invert_yaxis()
-        ax_for_non_engram = sns.heatmap(self.non_engram_df, ax=axes[2], robust=True, vmin=self.SCALE_MIN, vmax=self.SCALE_MAX, square=True, cmap=self.HEATMAP_CMAP)
+
+        ax_for_non_engram = sns.heatmap(self.non_engram_df, ax=axes[1], robust=True, vmin=self.SCALE_MIN, vmax=self.SCALE_MAX, square=True, cmap=self.HEATMAP_CMAP)
+        axes[1].set_xticks(self.time_ticks)
+        axes[1].set_xticklabels(self.time_ticks, fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+        axes[1].set_xlabel('Reference Time (s)', fontsize=self.DEFAULT_FONTSIZE)
+        axes[1].set_yticks(self.time_ticks)
+        axes[1].set_yticklabels(self.time_ticks, fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+        axes[1].set_title('Non-engram cells', fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+
+        color_bar_for_non_engram = ax_for_non_engram.collections[0].colorbar
+        color_bar_for_non_engram.ax.tick_params(labelsize=self.DEFAULT_FONTSIZE)
         ax_for_non_engram.invert_yaxis()
-        plt.show()
 
-    def view_correlation_colormesh(self):
-        fig, axes = plt.subplots(ncols=3, sharey=True, figsize=(25, 7))
+        ax_for_shuffle = sns.heatmap(self.shuffle_df, ax=axes[2], robust=True, vmin=self.SCALE_MIN, vmax=self.SCALE_MAX,
+                                     square=True, cmap=self.HEATMAP_CMAP)
+        axes[2].set_xticks(self.time_ticks)
+        axes[2].set_xticklabels(self.time_ticks, fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+        axes[2].set_xlabel('Reference Time (s)', fontsize=self.DEFAULT_FONTSIZE)
+        axes[2].set_yticks(self.time_ticks)
+        axes[2].set_yticklabels(self.time_ticks, fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
+        axes[2].set_title('Shuffled cells', fontsize=self.DEFAULT_FONTSIZE, fontweight=self.DEFAULT_FONT_WEIGHT)
 
-        self.__view_colormesh(self.shuffle_df, axes[0], 'all cells')
-        self.__view_colormesh(self.engram_df, axes[1], 'engram cells')
-        a = self.__view_colormesh(self.non_engram_df, axes[2], 'non-engram cells')
-        plt.colorbar(a, orientation="vertical")
+        color_bar_for_shuffle = ax_for_shuffle.collections[0].colorbar
+        color_bar_for_shuffle.ax.tick_params(labelsize=self.DEFAULT_FONTSIZE)
+        ax_for_shuffle.invert_yaxis()
 
-        plt.show()
+        fig.show()
 
     def view_sum_of_correlation_bar(self):
-        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(24, 15))
-
-        shuffle_mean = self.shuffle_df.mean()
-        engram_mean = self.engram_df.mean()
-        non_engram_mean = self.non_engram_df.mean()
-
-        axes[0][0].plot(shuffle_mean, label='all cells')
-        axes[0][0].plot(engram_mean, label='engram cells')
-        axes[0][0].plot(non_engram_mean, label='non-engram cells')
-        axes[0][0].legend()
+        fig, ax = plt.subplots(1, figsize=(5, 5))
 
         abs_shuffle_sum = self.shuffle_df.abs().sum()
         abs_engram_sum = self.engram_df.abs().sum()
         abs_non_engram_sum = self.non_engram_df.abs().sum()
 
-        axes[0][1].plot(abs_shuffle_sum, label='all cells')
-        axes[0][1].plot(abs_engram_sum, label='engram cells')
-        axes[0][1].plot(abs_non_engram_sum, label='non-engram cells')
-        axes[0][1].legend()
+        ax.plot(abs_shuffle_sum, label='shuffled cells')
+        ax.plot(abs_engram_sum, label='engram cells')
+        ax.plot(abs_non_engram_sum, label='non-engram cells')
+        ax.legend(loc='lower right')
 
-        sns.distplot(abs_engram_sum, bins=5, label='engram cells', ax=axes[1][0])
-        sns.distplot(abs_non_engram_sum, bins=5, label='non-engram cells', ax=axes[1][0])
-        axes[1][0].legend()
-
-        engram_sum_df = pd.DataFrame(abs_engram_sum).assign(Cell='engram cells')
-        non_engram_sum_df = pd.DataFrame(abs_non_engram_sum).assign(Cell='non-engram cells')
-        concat_df = pd.concat([engram_sum_df, non_engram_sum_df])
-        data = pd.melt(concat_df, id_vars=['Cell'], var_name=['Number'], value_name='sum of correlation')
-        sns.violinplot(x='Cell', y='sum of correlation', data=data, ax=axes[1][1])
-        sns.swarmplot(x='Cell', y='sum of correlation', data=data, ax=axes[1][1], color='0.5')
+        ax.set_title('Variation from shuffled cells', fontsize=16)
+        ax.set_ylabel('Sum of correlations', fontsize=14)
+        ax.set_xlabel('Reference time (s)', fontsize=14)
         fig.show()
-
-    def __calc_colormesh_maxmin(self):
-        all_average = self.calc_diagonal_average(self.shuffle_df)
-        engram_average = self.calc_diagonal_average(self.engram_df)
-        non_engram_average = self.calc_diagonal_average(self.non_engram_df)
-
-        return np.array([all_average, engram_average, non_engram_average]).mean()
-
-    def __calc_minmax(self):
-        all_max = self.shuffle_df.max().max()
-        engram_max = self.engram_df.max().max()
-        non_engram_max = self.non_engram_df.max().max()
-
-        max_value = max(all_max, engram_max, non_engram_max)
-
-        all_min = self.shuffle_df.min().min()
-        engram_min = self.engram_df.min().min()
-        non_engram_min = self.non_engram_df.min().min()
-
-        min_value = min(all_min, engram_min, non_engram_min)
-
-        return max_value, min_value
-
-    @staticmethod
-    def calc_diagonal_average(df):
-        seconds = len(df)
-        return (df * np.eye(seconds)).sum().mean()
-
-    @staticmethod
-    def scale_df(shuffle_df, engram_df, non_engram_df):
-        seconds = len(non_engram_df)
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaler.fit(non_engram_df)
-        shuffle_df = scaler.transform(shuffle_df)
-        shuffle_df = pd.DataFrame(shuffle_df.astype(float), columns=list(range(seconds)), index=list(range(seconds)))
-        engram_df = scaler.transform(engram_df)
-        engram_df = pd.DataFrame(engram_df.astype(float), columns=list(range(seconds)), index=list(range(seconds)))
-        non_engram_df = scaler.transform(non_engram_df)
-        non_engram_df = pd.DataFrame(non_engram_df.astype(float), columns=list(range(seconds)), index=list(range(seconds)))
-
-        return [shuffle_df, engram_df, non_engram_df]
-
-    @staticmethod
-    def __view_colormesh(df, axe, title=''):
-        x = y = np.arange(0, len(df))
-        X, Y = np.meshgrid(x, y)
-
-        df = df.astype(float)
-        axe.set_title(title)
-        return axe.pcolormesh(X, Y, df, shading='auto', cmap='bwr', vmin=-1.0, vmax=1.0)
