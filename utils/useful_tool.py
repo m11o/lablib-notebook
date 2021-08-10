5f6ac10da10f65b8850647b0420689a41216eddb
masahito1997 <velonica.gale2227@gmail.com>
2021-08-10 16:24:25 +0900

    refactor

--- utils/useful_tool.py
+++ utils/useful_tool.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def find_all_nan_cells(matrix: pd.DataFrame) -> list:
     nan_cells = []
     for cell_name, items in matrix.iteritems():
@@ -9,12 +10,14 @@ def find_all_nan_cells(matrix: pd.DataFrame) -> list:
 
     return nan_cells
 
+
 def dropped_unique_cells(cells1, cells2):
     dropped_cells = np.array([])
     dropped_cells = np.append(dropped_cells, cells1)
     dropped_cells = np.append(dropped_cells, cells2)
     dropped_cells = np.unique(dropped_cells)
     return dropped_cells
+
 
 def calc_event_rate(spikes, second, threshold=0.0):
     return len(spikes[spikes > threshold]) / second
