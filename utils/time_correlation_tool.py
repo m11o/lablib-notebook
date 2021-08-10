3a30c73579181d75a85af70ab13441e2cb2dd5a6
masahito1997 <velonica.gale2227@gmail.com>
2021-08-10 16:37:11 +0900

    create tools for time correlation

---
+++ utils/time_correlation_tool.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def calculate_cell_correlation(df, frame_index, size=9):
+    sliding_window = df.iloc[frame_index:frame_index + size, :]
+    return sliding_window.corr()
+
+
+def calculate_time_correlation(seconds, time_correlation_df, cell_correlation_dfs):
+    for i in range(seconds):
+        i_time_data = cell_correlation_dfs[i]
+
+        for j in range(seconds):
+            j_time_data = cell_correlation_dfs[j]
+
+            if time_correlation_df.at[j, i] is not np.nan:
+                time_correlation_df.at[i, j] = time_correlation_df.at[j, i]
+                continue
+
+            cell_count = len(i_time_data)
+            calculated_values = i_time_data * j_time_data
+            corr_sum = (calculated_values - (calculated_values * np.eye(len(calculated_values)))).sum().sum()
+            if time_correlation_df.at[i, j] is np.nan:
+                time_correlation_df.at[i, j] = 0.0
+            time_correlation_df.at[i, j] += corr_sum / (cell_count * (cell_count - 1))
