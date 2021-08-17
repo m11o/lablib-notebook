8a1f01d0287783ca484809dc9c07dfe17a697c5a
masahito1997 <velonica.gale2227@gmail.com>
2021-08-17 12:38:35 +0900

    refactor shuffle methods for shuffle cell correlations

--- utils/time_correlation_tool.py
+++ utils/time_correlation_tool.py
@@ -1,10 +1,30 @@
 import numpy as np
 import pandas as pd
 
+from math import sqrt
+
 
 def calculate_cell_correlation(df, frame_index, size=9):
     sliding_window = df.iloc[frame_index:frame_index + size, :]
-    return sliding_window.corr()
+    corr = sliding_window.corr()
+    corr.fillna(0.0, inplace=True)
+    return corr
+
+
+def build_cell_correlation_df(df, start, end):
+    seconds = end - start
+    cell_correlations_df = pd.DataFrame(
+        columns=list(range(len(df.columns) ** 2)),
+        index=list(range(seconds))
+    )
+    for index, second in enumerate(range(start, end)):
+        frame_index = second * 10
+        corr_df = calculate_cell_correlation(df, frame_index)
+
+        corr_df = corr_df - (corr_df * np.eye(len(corr_df)))
+        cell_correlations_df.iloc[index, :] = corr_df.to_numpy().ravel()
+
+    return cell_correlations_df
 
 
 def calculate_time_correlation(seconds, time_correlation_df, cell_correlation_dfs):
@@ -24,6 +44,11 @@ def calculate_time_correlation(seconds, time_correlation_df, cell_correlation_df
             if time_correlation_df.at[i, j] is np.nan:
                 time_correlation_df.at[i, j] = 0.0
             time_correlation_df.at[i, j] += corr_sum / (cell_count * (cell_count - 1))
+
+
+def calculate_shuffle_time_correlation(cell_correlation_df):
+    cell_size = sqrt(len(cell_correlation_df.columns))
+    return (np.dot(cell_correlation_df, cell_correlation_df.T)) / (cell_size * (cell_size - 1))
 
 
 def calculate_quantiles(quantile_value, df, columns=None) -> pd.Series:
