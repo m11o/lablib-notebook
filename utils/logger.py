04dbf175257a781b0f39ee9a746c212541b53183
masahito1997 <velonica.gale2227@gmail.com>
2021-08-17 09:24:07 +0900

    refactor logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,11 @@
 import logging
-import sys
 
 logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='[%(levelname)s]%(asctime)s: %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S'
 )
 
-logger = logging.getLogger('LOGGER_NAME')
+
+logger = logging.getLogger(__name__)
 
